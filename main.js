4ba5ef2959013077818a925e6acfb955530879c1
Gregory Czeslawiak <grzegorzczeslawiak@gmail.com>
2022-10-17 01:03:25 +0200

    html and css completed

---
+++ main.js
@@ -0,0 +1,4 @@
+const currentNumber = document.querySelector('.currentNumber');
+const previousNumber = document.querySelector('.previousNumber p');
+const mathSign = document.querySelector('.mathSign');
+const numbersButtons =  
